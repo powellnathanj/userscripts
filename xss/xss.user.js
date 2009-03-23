12e8bb29dceb9b6ea4f61698761ddaebd9ca72d0
npowell <npowell@franciene.localdomain>
2009-03-22 21:15:10 -0400

    Encoded the url, so the web service would handle &

--- xss/xss.user.js
+++ xss/xss.user.js
@@ -8,11 +8,12 @@
 // @exclude       nathanpowell.org
 // ==/UserScript==
 
-// Change this to whatever you like
+// Change these to whatever you like
 // I am using the Sinatra app you can find at:
 // http://github.com/slaney/wherehaveyoubeen
 var webserviceSend = 'http://wherehaveyoubeen.nathanpowell.org/here';
 var jqueryurl      = 'http://wherehaveyoubeen.nathanpowell.org/js/jquery-latest.js';
+var encodingurl    = 'http://wherehaveyoubeen.nathanpowell.org/js/urlEncode.js';
 
 // Almost all of this was ruthelessly stolen with permission from:
 // http://joanpiedra.com/jquery/greasemonkey/
@@ -20,6 +21,13 @@ var GM_JQ = document.createElement('script');
 GM_JQ.src = jqueryurl;
 GM_JQ.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(GM_JQ);
+
+// Also add the encoding stuff
+var add_encoding = document.createElement('script');
+add_encoding.src = encodingurl;
+add_encoding.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(add_encoding);
+
 
 function GM_wait() {
   if(typeof unsafeWindow.jQuery == 'undefined') { 
@@ -32,10 +40,11 @@ function GM_wait() {
 GM_wait();
 
 function runCode(){
+  var data = $.URLEncode(window.location.href);
   $.ajax({
           type: 'GET',
           dataType: 'jsonp',
           url: webserviceSend,
-          data: 'url=' + window.location,
+          data: 'url=' + data,
           });
 }
